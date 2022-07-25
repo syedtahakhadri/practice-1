2d0f71d04003e38965c42b22fcb471ff72365e66
hamed <abdulhamed341@gmail.com>
2022-07-25 14:58:25 +0530

    new mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace New_folder__2_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
